90929bd314c02d80e9feb5ada7fc236c172375bc
mlaakso@dev <laakso.mavrick@gmail.com>
2018-10-21 19:26:05 -0400

    api calls working between spa and api

--- spa/src/views/Home.vue
+++ spa/src/views/Home.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import axios from 'axios';
+
+// tslint:disable
 
 @Component({
   components: {
     HelloWorld,
+  },
+  async mounted() {
+    console.log('requesting data...');
+    // how to handle this in local dev? if prod, domain. if local, localhost
+    const response = await axios.get('http://35.203.34.230');
+    console.log(response);
   },
 })
 export default class Home extends Vue {}
