53c7d60aeb822fcfe04a9315f2993698a419a17e
mlaakso@dev <laakso.mavrick@gmail.com>
2018-10-28 17:11:12 -0400

    fix for failing auth test

--- test/integration/auth.e2e-spec.ts
+++ test/integration/auth.e2e-spec.ts
@@ -5,15 +5,19 @@ import { create, factories } from '../../lib/factory';
 import { AppModule } from '../../src/app/app.module';
 import { buildApp } from '../../src/main';
 import { User } from '../../src/user/user.entity';
+import { UserService } from '../../src/user/user.service';
 import { cleanDatabase } from '../test.utils';
 
 describe('Auth (e2e)', () => {
     let app: INestApplication;
+    let userService: UserService;
 
     beforeAll(async () => {
         const moduleFixture = await Test.createTestingModule({
             imports: [AppModule]
         }).compile();
+
+        userService = moduleFixture.get<UserService>(UserService);
 
         const raw = moduleFixture.createNestApplication();
         app = buildApp(raw);
@@ -21,16 +25,14 @@ describe('Auth (e2e)', () => {
     });
 
     it('can authorize a user with a valid password', async () => {
-        // const user = await create(User);
-        // const payload = {
-        //     email: 'abc@123.ca',
-        //     password: 'password'
-        // };
-        // const response = await request(app.getHttpServer())
-        //     .post('/auth')
-        //     .send(payload);
-        // expect(response.status).toEqual(200);
-        return true;
+        const payload = factories.User();
+        await userService.create(payload);
+        const response = await request(app.getHttpServer())
+            .post('/auth')
+            .send(payload);
+        expect(response.status).toEqual(201);
+        expect(response.body.token).toBeDefined();
+        expect(response.body.email).toEqual(payload.email);
     });
 
     it('can 401 a user with an invalid password', async () => {
